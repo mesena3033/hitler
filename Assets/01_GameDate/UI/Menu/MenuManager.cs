6d6e855f78e658d454aec9b76a42be3d42b8b84c
mesena3033 <kss-24210052@nsgcl.jp>
2026-05-19 15:33:50 +0900

    メニュー下書き

--- Assets/01_GameDate/UI/Menu/MenuManager.cs
+++ Assets/01_GameDate/UI/Menu/MenuManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class SettingMenu : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
