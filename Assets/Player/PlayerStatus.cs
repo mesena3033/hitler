3717c0b10581c68a45be84161f46e0f90b80c9d5
Gascogne <sou.mi.2190@icloud.com>
2026-05-22 12:30:04 +0900

    Playerprefab

--- Assets/Player/PlayerStatus.cs
+++ Assets/Player/PlayerStatus.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // HP
+    [SerializeField] private int maxHP = 100;
+    [SerializeField] private int currentHP;
+
+    // 攻撃力
+    [SerializeField] private float attackPower = 10f;
+    
+    // 防御力
+    [SerializeField] private float defensePower = 5f;
+
     void Start()
     {
-        
+        currentHP = maxHP;
+
     }
 
     // Update is called once per frame
