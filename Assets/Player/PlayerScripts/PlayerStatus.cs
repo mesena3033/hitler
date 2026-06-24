d7742d671e73533891b3dd0576f6fcadcbab2959
Gascogne <sou.mi.2190@icloud.com>
2026-06-24 13:36:14 +0900

    不足投げ

--- Assets/Player/PlayerScripts/PlayerStatus.cs
+++ Assets/Player/PlayerScripts/PlayerStatus.cs
@@ -124,6 +124,9 @@ public class PlayerStatus : MonoBehaviour
             ApplyDamage(dmg);
         }
     }
+
+    
+
     /// <summary>
     /// ////////////////////////実装前に消す/////////////////////////////////////
     /// </summary>
