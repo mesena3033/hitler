4160c094edce838cedf1a0515c1dabe62289cc3f
Gascogne <sou.mi.2190@icloud.com>
2026-06-29 13:29:24 +0900

    諸々変更

--- Assets/Player/PlayerScripts/PlayerAttack.cs
+++ Assets/Player/PlayerScripts/PlayerAttack.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -49,6 +50,7 @@ public class PlayerAttack : MonoBehaviour
 
     void Update()
     {
+        //move.CanNotMoving();
         if (status.IsPlayerDead) return;
 
         if (!move.IsDodging && !move.IsBeingHit
@@ -128,6 +130,9 @@ public class PlayerAttack : MonoBehaviour
         // 攻撃時に剣の当たり判定を有効化
         if (swordCollider != null) swordCollider.enabled = true;
 
+        // オブジェクトに当たったら判定を無効化
+        //if()
+
     }
 
 
@@ -199,6 +204,16 @@ public class PlayerAttack : MonoBehaviour
         // Debug.Log($"PlayerHP={playerHP} EnemyHP={enemyHP} PlayerATK={playerAtk} EnemyDEF={targetDefense} Damage={dmg}");
     }
 
+    public void OnWallHit()
+    {
+        if (swordCollider != null)
+        {
+            swordCollider.enabled = false;
+        }
+        
+
+    }
+
 
     // 攻撃系スキルメソッド
     public int SkillDamage(int amount)
@@ -222,7 +237,6 @@ public class PlayerAttack : MonoBehaviour
         int dmg = 0;
 
         
-
         // 通常攻撃
         if (!isSkill)
         {
